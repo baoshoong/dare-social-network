c4f57dd259115764448b7bc0d30910e0dbba811f
PhatITpromax <phatvo779@gmail.com>
2024-08-11 18:56:20 +0700

    HTML, CSS - Login, Register, Search

--- client/social-network/src/app/page/layout/search/search.component.ts
+++ client/social-network/src/app/page/layout/search/search.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-
+  searchQuery: string = '';
 }
