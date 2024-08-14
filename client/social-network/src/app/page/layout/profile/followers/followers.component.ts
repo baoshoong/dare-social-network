2809fb6dbe002e0faba47dbe9aff85e08d8d6d1a
Khoi <huynhminhkhoi1207@gmail.com>
2024-08-14 08:39:55 +0700

    upload avatar profile

--- client/social-network/src/app/page/layout/profile/followers/followers.component.ts
+++ client/social-network/src/app/page/layout/profile/followers/followers.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import {NgForOf} from "@angular/common";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-followers',
   standalone: true,
   imports: [
     NgForOf,
-    MatButton
+    MatButton,
+    MatFormField,
+    MatIcon,
+    MatIconButton,
+    MatInput,
+    MatLabel,
+    MatSuffix
   ],
   templateUrl: './followers.component.html',
   styleUrl: './followers.component.scss'
