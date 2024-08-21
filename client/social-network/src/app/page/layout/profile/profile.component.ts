a9e1f656d62d1954e3ff4f630058ed54dd993d09
thanhhuy5902 <tranthanhhuy42@gmail.com>
2024-08-21 10:23:12 +0700

    create like and comment

--- client/social-network/src/app/page/layout/profile/profile.component.ts
+++ client/social-network/src/app/page/layout/profile/profile.component.ts
@@ -1,26 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { FollowingComponent } from './following/following.component';
 import { FollowersComponent } from './followers/followers.component';
 import { MaterialModule } from '../../../shared/material.module';
-import { NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf } from '@angular/common';
 import { PostComponent } from '../../../shared/components/post/post.component';
-import { PostModel } from '../../../model/post.model';
+import { PostModel, PostResponse } from '../../../model/post.model';
+import { Subscription } from 'rxjs';
+import { PostState } from '../../../ngrx/post/post.state';
+import { Store } from '@ngrx/store';
+import { ProfileState } from '../../../ngrx/profile/profile.state';
+import * as PostAction from '../../../ngrx/post/post.actions';
+import { ProfileModel } from '../../../model/profile.model';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MaterialModule, NgForOf, PostComponent],
+  imports: [MaterialModule, NgForOf, PostComponent, AsyncPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-  name: string = 'Current Name';
-  bio: string = 'Current Bio';
-  avatarUrl: string = 'https://www.w3schools.com/howto/img_avatar.png';
+export class ProfileComponent implements OnInit {
+  subscriptions: Subscription[] = [];
 
-  constructor(public dialog: MatDialog) {}
+  profileMine$ = this.store.select('profile', 'mine');
+
+  minePosts$ = this.store.select('post', 'minePosts');
+
+  minePosts: PostResponse = <PostResponse>{};
+  profileMine: ProfileModel = <ProfileModel>{};
+
+  constructor(
+    public dialog: MatDialog,
+    public store: Store<{
+      post: PostState;
+      profile: ProfileState;
+    }>,
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.profileMine$.subscribe((profile) => {
+        if (profile) {
+          this.profileMine = profile;
+          this.store.dispatch(
+            PostAction.getMinePost({
+              uid: this.profileMine.uid,
+              pageNumber: 1,
+              limitNumber: 5,
+            }),
+          );
+        }
+      }),
+
+      this.minePosts$.subscribe((posts) => {
+        console.log(posts);
+        this.minePosts = posts;
+      }),
+    );
+  }
 
   openDialog2() {
     this.dialog.open(FollowingComponent);
@@ -33,17 +72,18 @@ export class ProfileComponent {
   openEditProfileDialog(): void {
     const dialogRef = this.dialog.open(EditProfileComponent, {
       data: {
-        avatarUrl: this.avatarUrl,
-        name: this.name,
-        bio: this.bio,
+        avatarUrl: this.profileMine.avatarUrl,
+        name: this.profileMine.userName,
+        bio: this.profileMine.bio,
       },
     });
     dialogRef.componentInstance.avatarChanged.subscribe(
       (newAvatarUrl: string) => {
-        this.avatarUrl = newAvatarUrl;
-        this.name =
+        this.profileMine.avatarUrl = newAvatarUrl;
+        this.profileMine.userName =
           dialogRef.componentInstance.editProfileForm.value.name ?? '';
-        this.bio = dialogRef.componentInstance.editProfileForm.value.bio ?? '';
+        this.profileMine.bio =
+          dialogRef.componentInstance.editProfileForm.value.bio ?? '';
       },
     );
   }
---
+++ server/social-network/src/comment/dto/create-comment.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsString()
+  uid: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  postId: number;
+}
